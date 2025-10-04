2ad14ac17721844d7c707897839156e01fec34d6
kevin diaz <diazkevinmota@gmail.com>
2025-10-04 03:19:08 -0400

    Actualiza el README.md para incluir información sobre la sección de idiomas y mejorar la claridad del contenido existente.

---
+++ pages/500.js
@@ -0,0 +1,4 @@
+// pages/500.js
+export default function Custom500() {
+    return <h1>500 -aqui ocurrio un error en el servidor por lo tanto no se puede encontrar la pagina por favor intentelo mas tarde y verifique si esta funcionando correctamente</h1>;
+  }
